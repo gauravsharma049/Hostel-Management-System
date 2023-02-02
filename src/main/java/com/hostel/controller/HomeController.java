61e3cb7e955ade7c243d42f8c9d0423003bb5fcd
Gaurav Sharma <gauravsharma049049@gmail.com>
2023-02-03 01:03:00 +0530

    initialized the project

---
+++ src/main/java/com/hostel/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.hostel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/not-found")
+    public String notFound() {
+        return "404.html";
+    }
+}

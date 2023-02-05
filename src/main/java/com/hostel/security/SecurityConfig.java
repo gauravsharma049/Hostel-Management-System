95a68100bc3b573f936bdbe8b8f92a6d87d6e328
Gaurav Sharma <gauravsharma049049@gmail.com>
2023-02-05 17:47:40 +0530

    implemented security

--- src/main/java/com/hostel/security/SecurityConfig.java
+++ src/main/java/com/hostel/security/SecurityConfig.java
@@ -1,5 +1,63 @@
 package com.hostel.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Bean
+    public UserDetailsService getUserDetailsService(){
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(this.passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.authenticationProvider(this.authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/warden/**").hasAnyRole("ChiefWarden", "Warden")
+            .antMatchers("/student/**").hasRole("student")
+            .antMatchers("/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .loginProcessingUrl("/login")
+            .failureUrl("/?error")
+            .and()
+            .logout()
+            .logoutSuccessUrl("/login")
+            .and()
+            .csrf()
+            .disable();
+            
+    }
+
+    
     
 }
--- src/main/java/com/hostel/security/UserDetailsServiceImpl.java
+++ src/main/java/com/hostel/security/UserDetailsServiceImpl.java
@@ -1,15 +1,23 @@
 package com.hostel.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class UserDetailsServiceImpl implements UserDetailsService{
+import com.hostel.model.User;
+import com.hostel.repository.UserRepository;
 
+public class UserDetailsServiceImpl implements UserDetailsService{
+    @Autowired
+    private UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new UsernameNotFoundException("user "+email+" not found");
+        }
+        else return new CustomUserDetails(user);
     }
     
 }
